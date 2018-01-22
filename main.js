52494c342b27593bae07f54647bbbb412729181f
Bruce Hsu <brucehsu@fontrip.com>
2018-01-22 16:52:55 +0800

    modify

--- main.js
+++ main.js
@@ -3,21 +3,41 @@ const url = require('url');
 const path = require('path');
 const rp = require('request-promise');
 let win;
-let accessToken;
+
+global.sharedObj = { accessToken: null };
 
 const loginRrequestOptions = {
     method: 'POST',
     uri: 'https://test-platform.welcometw.com/api/fontrip/kiosk/kioskLogin',
+    headers: {
+        'User-Agent': 'insomnia/5.12.4',
+        'Content-Type': 'application/json',
+        'secret': '4yXnWw4ggccCxA8482wJ',
+        'key': '779473641250'
+    },
     body: {
-        some: 'payload'
+        "basic": {
+            "appName": "kiosk",
+            "appVersion": "0.5",
+            "lang": "zh_TW",
+            "os": "kiosk",
+            "deviceId": "kiosktest",
+            "latitude": "25.00",
+            "longitude": "123.21"
+        },
+        "request": {
+            "username": "kiosk@fontrip.com.tw",
+            "password": "20170901",
+            "accountCode": "ST64386174"
+        }
     },
     json: true // Automatically stringifies the body to JSON
 };
 
 const browserWindowOptions = {
     // width: 800, height: 600,
-    // fullscreen: true,
-    kiosk: true,
+    fullscreen: true,
+    // kiosk: true,
 }
 
 function createMain() {
@@ -43,7 +63,19 @@ function createMain() {
 
 }
 
-app.on('ready', createMain);
+app.on('ready', () => {
+    console.log('app.ready()');
+    rp(loginRrequestOptions)
+        .then(function (data) {
+            console.log('data.response.success : ' + data.response.success);
+            global.sharedObj.accessToken = data.result.accessToken;
+            createMain();
+        })
+        .catch(function (err) {
+            console.error(err);
+            // API call failed...
+        });
+});
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
